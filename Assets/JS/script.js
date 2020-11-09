9fdf808eef6c292a40d6f83df5aa650a230495f9
Garrett Cox <gacx89@gmail.com>
2020-11-09 10:12:22 -0600

    added call to mapbox api inside of displayMap() function in order to perform forward geocode of searched zipcode

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -21,6 +21,7 @@ var getBusinesses = function (zipCode) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
+          displayMap(zipCode);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -30,6 +31,34 @@ var getBusinesses = function (zipCode) {
       // Alert user if fetch fails. This will be changed to modal. Disconnect ethernet/wifi adapter in order to test.
       console.log("Unable to connect to database");
     });
+
+
+};
+
+var displayMap = function (zipCode) {
+  fetch ("https://api.mapbox.com/geocoding/v5/mapbox.places/" + zipCode + ".json?country=US&types=postcode&access_token=pk.eyJ1IjoibmlnaHRwaWNuaWMiLCJhIjoiY2toMHpyanQ2MTIwcTJwcGcxdXM1Y25zcyJ9.3s7JF30Ao0zbepk-0kKMMw")
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        alert ("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.log("Unable to connect to database");
+    });
+
+  /*mapboxgl.accessToken =
+  "pk.eyJ1IjoibmlnaHRwaWNuaWMiLCJhIjoiY2toM3hzcTQzMDZqZjJxbnd5dG56amFpOCJ9.RGnPCTAZ3oZvzUcyfQJvjQ";
+  var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+  center: ["https://api.mapbox.com/geocoding/v5/mapbox.places/2717&S&LAMAR&BLVD&APT&2036.json?types=address&access_token=pk.eyJ1IjoibmlnaHRwaWNuaWMiLCJhIjoiY2toMHpyanQ2MTIwcTJwcGcxdXM1Y25zcyJ9.3s7JF30Ao0zbepk-0kKMMw"], 
+  
+  zoom: 9, // starting zoom
+  });*/
 };
 
 // Update HTML elements and display businesses in cards along with creating map
